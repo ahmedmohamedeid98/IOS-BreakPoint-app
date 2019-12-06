0748705474c129fe7830f66cdc0992543aa9f59f
ahmed <ahmed@ahmeds-Mac.local>
2019-12-05 17:32:39 -0800

    setup user Auth UI

---
+++ BreakPoint/Controllers/UserAuthVC.swift
@@ -0,0 +1,30 @@
+//
+//  UserAuthVC.swift
+//  BreakPoint
+//
+//  Created by ahmed on 12/5/19.
+//  Copyright © 2019 ahmed. All rights reserved.
+//
+
+import UIKit
+
+class UserAuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
